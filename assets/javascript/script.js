3a83443dbf7870a2b2eddae32127df13f11f51c7
PedroGiacon <pedrog.giacon@gmail.com>
2023-03-28 16:56:48 -0300

    Scroll fix

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,8 @@
-// Este código adiciona um evento de rolagem à janela do navegador. Quando a janela é rolada, a função passada como segundo argumento é executada.
-window.addEventListener('scroll', function(){
-    // Dentro da função, o código obtém o elemento HTML com a classe "navegacao" usando o método querySelector e o armazena em uma variável chamada "navegacao".
-    var navegacao = document.querySelector('.navegacao');
-    // A próxima linha de código usa o método classList.toggle para adicionar ou remover a classe "sticky" do elemento "navegacao", dependendo da posição da rolagem da janela.
-    navegacao.classList.toggle('sticky', window.scrollY > 135);
-  })
+window.onscroll = function() {
+  var navegacao = document.querySelector('.navegacao');
+  if (window.pageYOffset > navegacao.offsetTop) {
+     navegacao.classList.add('sticky');
+  } else {
+     navegacao.classList.remove('sticky');
+  }
+}
