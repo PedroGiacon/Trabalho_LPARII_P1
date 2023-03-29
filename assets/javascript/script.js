179440071d4e7ce832fbeade8e3d062f272eb543
Julio César Da Silva <jnb.py13@hotmail.com>
2023-03-29 09:45:14 -0300

    Atualização header

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,8 +1,12 @@
 window.onscroll = function() {
+  var divElemento = document.querySelector(".logocentro");
   var navegacao = document.querySelector('.navegacao');
   if (window.pageYOffset > navegacao.offsetTop) {
      navegacao.classList.add('sticky');
+     divElemento.classList.add('desativo');
+
   } else {
      navegacao.classList.remove('sticky');
+     divElemento.classList.remove('desativo');
   }
 }
